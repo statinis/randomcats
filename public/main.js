78274562d73ae7b8c8070de0eae99c2cca1ff369
maxswebservices <93405617+maxswebservices@users.noreply.github.com>
2021-12-08 17:07:12 -0800

    Create main.js

---
+++ public/main.js
@@ -0,0 +1,23 @@
+const mainWin = document.querySelector(".main");
+const img = document.querySelector(".img");
+getCatGif();
+
+//fetch catgif
+function getCatGif() {
+  fetch("https://meme-api.herokuapp.com/gimme/CatGifs")
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((result) => {
+      console.log(result.url);
+      loader();
+      setTimeout(() => {
+        img.setAttribute("src", result.url);
+      }, 1000);
+    });
+}
+function loader() {
+  var url1 = "loading.gif";
+  img.setAttribute("src", url1);
+}
